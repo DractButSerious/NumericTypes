40420c11dca1232eea396af2b41ca678095a0e6c
DractButSerious <159930982+DractButSerious@users.noreply.github.com>
2024-10-22 19:30:46 +0000

    Published.

---
+++ NumTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// RectangleAreaCalculator.Program.Main();
+
+namespace RectangleAreaCalculator
+{
+    class Program
+    {
+        internal static void Main()
+        {
+            // Prompt the user for the length.
+            Console.WriteLine("Enter the length of the rectangle:");
+            double length = double.Parse(Console.ReadLine());
+
+            // Prompt the user for the width.
+            Console.WriteLine("Enter the width of the rectangle:");
+            double width = double.Parse(Console.ReadLine());
+
+            // Calculate the area.
+            double area = length * width;
+
+            // Present the length to the user.
+            Console.WriteLine("The area of the rectangle is: " + area);
+        }
+    }
+}
